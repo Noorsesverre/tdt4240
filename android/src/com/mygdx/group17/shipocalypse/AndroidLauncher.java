08da2a15a009b78e905d75efde0a7024e1a0f420
Sverre Francis Uijting <sf.uijting@hotmail.no>
2024-03-02 10:41:12 +0100

    Init commit

---
+++ android/src/com/mygdx/group17/shipocalypse/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.group17.shipocalypse;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.group17.shipocalypse.Shipocalypse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Shipocalypse(), config);
+	}
+}
