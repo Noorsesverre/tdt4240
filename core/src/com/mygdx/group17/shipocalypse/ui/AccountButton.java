bb0a73852273c0d8894892e28518ff38274a0bcf
android9999 <dinicola.andy@gmail.com>
2024-04-06 15:33:36 +0200

    I am trying to implement a new state which should show the account options like sign in or log in or skip, and I need this AcountState because in the MenuButtons there are not functions for log in or sign in or skip, but now the app crashesh for some reason and I do not why, I also tried to make a panel, something that show up when you tap a button but without any success, I also made a script called AccountManager that will be use for managing the log in and the sign in of the user

--- core/src/com/mygdx/group17/shipocalypse/ui/AccountButton.java
+++ core/src/com/mygdx/group17/shipocalypse/ui/AccountButton.java
@@ -1,4 +1,19 @@
 package com.mygdx.group17.shipocalypse.ui;
 
-public class AccountButton {
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class AccountButton extends Button{
+    public AccountButton(ShapeRenderer shape, int x, int y) {
+        super(shape, x, y);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
